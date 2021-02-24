c47043692e84857f887363b931eadccc3c42c2ac
Gregory Hoffman <gah32927@creighton.edu>
2021-02-24 15:15:55 -0600

    commit1

---
+++ forms/pets/pets.js
@@ -0,0 +1,11 @@
+petNames = ["Darcy", "Jack", "Jerry", "Marty", "Vito", "Vinny", "Riley", "Saddie", "Marge", "Sam"]
+
+withSal = [petNames, "Sal"]
+
+withSal = petNames.push("Sal") 
+
+console.log(`The third pets name is ${petNames[2]}`)
+
+console.log(`The array has ${petNames.length} names in it.`)
+
+petNames.pop(1)
